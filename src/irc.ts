29b7d0fc577a290e847156528139f45b048fec9a
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2020-03-17 19:18:43 +0100

    feat: IRC port config via environment variable

--- src/irc.ts
+++ src/irc.ts
@@ -4,9 +4,15 @@ import { env } from "process";
 const channel = env.IRC_CHANNEL || "#flix";
 const server = env.IRC_SERVER || "irc.r";
 export const nick = env.IRC_NICK || "bruellwuerfel";
+let port = 6667;
+
+try {
+  port = parseInt(env.IRC_PORT || "");
+} catch (error) {}
 
 const ircClient = new Client(server, nick, {
-  channels: [channel]
+  channels: [channel],
+  port: port
 });
 
 export const send = (userName: string, message: string) =>
