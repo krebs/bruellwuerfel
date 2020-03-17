e0a183cc520b59bde5b3b96ede56205e1da9a181
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2020-03-17 22:43:57 +0100

    fix: no slash

--- src/templates/javascript.ts
+++ src/templates/javascript.ts
@@ -28,7 +28,7 @@ const renderShoutbox = div => {
     const messageText = input.value;
 
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", "/messages", true);
+    xhr.open("POST", "messages", true);
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.onload = () => (input.value = "");
     xhr.send(JSON.stringify({ message: messageText }));
@@ -41,7 +41,7 @@ const renderShoutbox = div => {
 
 (function refresher() {
   const xhr = new XMLHttpRequest();
-  xhr.open("GET", "/messages?limit=10", true);
+  xhr.open("GET", "messages?limit=10", true);
   xhr.onload = e => {
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
